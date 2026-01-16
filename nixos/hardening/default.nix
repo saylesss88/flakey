c58d01a4ed09d6c3f77a0528b0ac8dffa82a8030
saylesss88 <saylesss87@proton.me>
2026-01-16 14:53:25 -0500

    pam.services.swaylock so i can get back in

--- nixos/hardening/default.nix
+++ nixos/hardening/default.nix
@@ -2,7 +2,8 @@
   pkgs,
   lib,
   ...
-}: {
+}:
+{
   imports = [
     ./security.nix
     ./systemd.nix
@@ -27,38 +28,48 @@
 
   services.displayManager.autoLogin.enable = false;
 
-  security.wrappers = {
-    # Remove unnecessary SUID binaries
-    # fusermount.setuid = lib.mkForce false;
-    # fusermount3.setuid = lib.mkForce false;
-    # mount.setuid = lib.mkForce false;
-    # umount.setuid = lib.mkForce false;
-    pkexec.setuid = lib.mkForce false;
-    su.setuid = lib.mkForce false;
-    sudo.setuid = lib.mkForce false;
-    sudoedit.setuid = lib.mkForce false;
-    sg.setuid = lib.mkForce false;
-    newgrp.setuid = lib.mkForce false;
-    bwrap = {
-      source = "${pkgs.bubblewrap}/bin/bwrap";
-      owner = "root";
-      group = "root";
-      setuid = true;
-    };
-    newuidmap = {
-      setuid = true;
-      owner = "root";
-      group = "root";
-      source = "${pkgs.shadow}/bin/newuidmap";
-    };
+  security = {
+    wrappers = {
+      # Remove unnecessary SUID binaries
+      # fusermount.setuid = lib.mkForce false;
+      # fusermount3.setuid = lib.mkForce false;
+      # mount.setuid = lib.mkForce false;
+      # umount.setuid = lib.mkForce false;
+      pkexec.setuid = lib.mkForce false;
+      su.setuid = lib.mkForce false;
+      sudo.setuid = lib.mkForce false;
+      sudoedit.setuid = lib.mkForce false;
+      sg.setuid = lib.mkForce false;
+      newgrp.setuid = lib.mkForce false;
+      bwrap = {
+        source = "${pkgs.bubblewrap}/bin/bwrap";
+        owner = "root";
+        group = "root";
+        setuid = true;
+      };
+      newuidmap = {
+        setuid = true;
+        owner = "root";
+        group = "root";
+        source = "${pkgs.shadow}/bin/newuidmap";
+      };
 
-    newgidmap = {
-      setuid = true;
-      owner = "root";
-      group = "root";
-      source = "${pkgs.shadow}/bin/newgidmap";
+      newgidmap = {
+        setuid = true;
+        owner = "root";
+        group = "root";
+        source = "${pkgs.shadow}/bin/newgidmap";
+      };
+      # newgidmap.setuid = lib.mkForce false;
+      # newuidmap.setuid = lib.mkForce false;
     };
-    # newgidmap.setuid = lib.mkForce false;
-    # newuidmap.setuid = lib.mkForce false;
+    pam.services.swaylock = {
+      text = ''
+        auth include login
+        account include login
+        password include login
+        session include login
+      '';
+    };
   };
 }
