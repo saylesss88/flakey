09b26a4d849dcfe565395aad763398c4475d5044
saylesss88 <saylesss87@proton.me>
2026-01-16 10:32:00 -0500

    users.nix

--- hosts/magic/configuration.nix
+++ hosts/magic/configuration.nix
@@ -13,6 +13,7 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ./users.nix
   ];
 
   # Use the systemd-boot EFI boot loader.
@@ -79,106 +80,6 @@
 
   # Set your time zone.
   time.timeZone = "America/New_York";
-
-  users.users.jr = {
-    isNormalUser = true;
-    # extraGroups = [ "wheel" ];
-    group = "jr";
-    shell = pkgs.zsh;
-    hashedPasswordFile = "/persist/etc/nixos-secrets/passwords/jr";
-  };
-
-  users.mutableUsers = false;
-
-  users.groups.jr = {
-
-  };
-
-  users.users.admin = {
-    isNormalUser = true;
-    description = "admin acct";
-    extraGroups = [ "wheel" ];
-    group = "admin";
-    # initialHashedPassword = "$y$j9T$l6BO3cf.TgxpnADw.4J5h/$3VlemmABgXtfcmYBFB7/gdxephephravjqF/YG/PQAA";
-    hashedPasswordFile = "/persist/etc/nixos-secrets/passwords/jr";
-  };
-
-  users.groups.admin = { };
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # services.pulseaudio.enable = true;
-  # OR
-  # services.pipewire = {
-  #   enable = true;
-  #   pulse.enable = true;
-  # };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.alice = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  #   packages = with pkgs; [
-  #     tree
-  #   ];
-  # };
-
-  # programs.firefox.enable = true;
-
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  # ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
--- hosts/magic/users.nix
+++ hosts/magic/users.nix
@@ -11,7 +11,7 @@
     uid = 1000;
     openssh = {
       authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDp1GILFjayKi/mKBMg36nDV8TyG+rZDXmNheYdOzA4N (none)"
+        # "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDp1GILFjayKi/mKBMg36nDV8TyG+rZDXmNheYdOzA4N (none)"
       ];
     };
     extraGroups = lib.mkForce [
@@ -25,7 +25,7 @@
       #  thunderbird
     ];
     shell = pkgs.zsh;
-    hashedPasswordFile = config.sops.secrets.password_hash.path;
+    hashedPasswordFile = "/persist/etc/nixos-secrets/passwords/jr";
   };
   users.mutableUsers = false;
 
@@ -40,7 +40,7 @@
     packages = with pkgs; [
       #  thunderbird
     ];
-    initialHashedPassword = "$y$j9T$iw3SD4j3NJaO42w3d28bz0$I24HkUwFfXn0Nk0dcZH.yoTcg93kCCn6hO.HRjSkuOD";
+    hashedPasswordFile = "/persist/etc/nixos-secrets/passwords/jr";
   };
 
   users.groups.admin = { };
